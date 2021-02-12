6a363be783954d78f5deaea0d4f791acec649657
Zero-Sharp <77262546+Zero-Sharp@users.noreply.github.com>
2021-02-12 16:17:46 +0900

    Update prelude.rs

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,4 @@
-pub use crate::graph::*:
+pub use crate::graph::*;
 pub use crate::min_cost_flow::{MinCostFlow,MinCostFlowST};
 // pub use crate::max_flow::MaxFlow;
 // pub use crate::shortest_path::ShortestPath;
