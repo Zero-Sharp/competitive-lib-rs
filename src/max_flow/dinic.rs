e929139ccaafa2b4662ee5198091d27a6b5ca844
Zero-Sharp <77262546+Zero-Sharp@users.noreply.github.com>
2021-03-05 23:33:06 +0900

    Update dinic.rs

--- src/max_flow/dinic.rs
+++ src/max_flow/dinic.rs
@@ -21,6 +21,178 @@ where A: AccGraph<Value = U>,
         }
     }
     let mut res: A = graph.clone();
+    while let Some(mut level_graph) = mk_level_graph(&res,s,t) {
+        let blocking_flow = mk_blocking_flow(&mut level_graph, s, t);
+        augment_along_blocking(blocking_flow, &mut flow, &mut res, graph);
+    }
+    flow
+}
+
+// return level graph, but each edge is inversed. 
+fn mk_level_graph<A,U>(
+    res: &A,
+    s: usize,
+    t: usize,
+) -> Option<A>
+where A: AccGraph<Value = U>,
+      U: Copy + Ord + Add + Sub<Output = U> + Zero + AddAssign + SubAssign,
+{
+    let mut lvl = A::new(res.size());
+    let mut now = vec![s];
+    let mut next = Vec::new();
+    let mut end = false;
+    let mut arrived = vec![false; res.size()];
+    arrived[s] = true;
+    loop {
+        let mut now_arrived = BTreeSet::new();
+        while let Some(from) = now.pop() {
+            for (to, val) in res.neighbors(from) {
+                if to == t {
+                    end = true;
+                }
+                if !arrived[to] {
+                    lvl.add_edge(to,from,val);
+                    now_arrived.insert(to);
+                    next.push(to);
+                }
+            }
+        }
+        if end {
+            break;
+        } else if !next.is_empty() {
+            swap(&mut now, &mut next);
+            for n in now_arrived {
+                arrived[n] = true;
+            }
+            continue;
+        } else {
+            return None
+        }
+    }
+    Some(lvl)
+}
+
+fn mk_blocking_flow<A,U>(
+    level_graph: &mut A,
+    s: usize,
+    t: usize,
+) -> A
+where A: AccGraph<Value = U>,
+      U: Copy + Ord + Add + Sub<Output = U> + Zero + AddAssign + SubAssign,
+{
+    let len = level_graph.size();
+    let mut ret = A::new(len);
+    let mut stack = vec![t];
+    let mut prev = vec![t;len];
+    while let Some(mut from) = stack.pop() {
+        for (mut to,mut val) in level_graph.neighbors(from) {
+            if to == s {
+                let mut flag = false;
+                let mut path = Vec::new();
+                let mut g = val;
+                loop {
+                    match level_graph.get(from,to) {
+                        Some(x) => val = x,
+                        None => {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    path.push((from,to));
+                    g = min(g,val);
+                    if from == t {
+                        break;
+                    }
+                    to = from;
+                    from = prev[from];
+                }
+                if flag {
+                    continue;
+                }
+                let minus = |x| {
+                    match x {
+                        Some(val) => {
+                            if val <= g {
+                                None
+                            } else {
+                                Some(val-g)
+                            }
+                        },
+                        None => unreachable!("mk_blocking_flow")
+                    }
+                };
+                for (from,to) in path {
+                    ret.modify(to,from, |x| Some(x.unwrap_or(U::zero())+g));
+                    level_graph.modify(from,to,minus);
+                }
+            } else {
+                prev[to] = from;
+                stack.push(to);
+            }
+        }
+    }
+    ret
+}
+
+fn augment_along_blocking<A,U>(
+    blocking_flow: A,
+    flow: &mut A,
+    res: &mut A,
+    graph: &A
+)
+where A: AccGraph<Value = U>,
+      U: Copy + Ord + Add + Sub<Output = U> + Zero + AddAssign + SubAssign,
+{
+    for from in 0..blocking_flow.size() {
+        for (to,g) in blocking_flow.neighbors(from) {
+            let plus = |x| {
+                match x {
+                    Some(val) => Some(val+g),
+                    None => Some(g)
+                }
+            };
+            let minus = |x| {
+                match x {
+                    Some(val) => {
+                        if val <= g {
+                            None
+                        } else {
+                            Some(val-g)
+                        }
+                    },
+                    None => unreachable!()
+                }
+            };
+            if graph.is_edge(from,to) {
+                flow.modify(from,to,plus);
+                res.modify(from,to,minus);
+                res.modify(to,from,plus);
+            } else {
+                flow.modify(to,from,minus);
+                res.modify(to,from,plus);
+                res.modify(from,to,minus);
+            }
+        } 
+    }
+}
+
+/*
+pub fn solve<A,U>(
+    graph: &A,
+    s: usize,
+    t: usize,
+) -> A
+where A: AccGraph<Value = U>,
+    U: Copy + Ord + Add + Sub<Output = U> + Zero + AddAssign + SubAssign,
+{
+    let size = graph.size();
+    let mut flow: A = graph.clone();
+    for from in 0..size {
+        for (to, _) in graph.neighbors(from) {
+            flow.add_edge(from,to,U::zero());
+        }
+    }
+    let mut res: A = graph.clone();
     let mut blockings = find_blocking(&res,s,t);
     while !blockings.is_empty() {
         for path in &blockings {
@@ -152,6 +324,7 @@ where A: AccGraph<Value = U>,
             }
         }
 }
+*/
 
 /*
 pub fn solve<A,U>(
